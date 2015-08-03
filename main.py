e8bbd30dcd9fc051aaa91ea72bdc86f68ea778f1
Marek <marek.kraft@gmail.com>
2015-08-03 15:18:06 +0200

    added blur and derivative computation

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ class EyeTracker:
         if self.display_results:
             cv2.namedWindow('results')
 
+    # TODO - split face, eye position and drawing into separate functions
     def get_face_pos(self):
+        # Prewitt filter masks
+        dx = np.array([[1.0, 0.0, -1.0],[1.0, 0.0, -1.0],[1.0, 0.0, -1.0],])
+        dy = np.transpose(dx)
         ret, img = self.video_capture.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.face_cascade.detectMultiScale(gray, 1.3, 15)
@@ -22,6 +26,12 @@ class EyeTracker:
             if self.display_results:
                 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             roi_gray = gray[y:y + h, x:x + w]
+            # filter with Gaussian
+            roi_gray_blurred = cv2.GaussianBlur(roi_gray, (5, 5), 0.05*h, 0.05*w)
+            cv2.imshow("blurred", roi_gray_blurred)
+            #compute partial derivatives
+            x_derivative = cv2.filter2D(roi_gray_blurred, cv2.CV_32F, dx)
+            y_derivative = cv2.filter2D(roi_gray_blurred, cv2.CV_32F, dy)
             roi_color = img[y:y + h, x:x + w]
             eyes = self.eye_cascade.detectMultiScale(roi_gray, 1.2, 10)
             if self.display_results:
